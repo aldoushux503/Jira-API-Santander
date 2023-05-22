8c1863325d8db0ccce7afd754f53531ed94aced9
aldoushux503 <elnikovnikita4@gmail.com>
2023-05-22 20:47:26 +0200

    Changed all Polish to English

--- src/main/java/org/example/SantanderJiraAPI.java
+++ src/main/java/org/example/SantanderJiraAPI.java
@@ -18,7 +18,7 @@ public class SantanderJiraAPI {
         String apiToken = "ATATT3xFfGF0D-taTAiRvnhU5Mem8K9w9vEzWSH-NHtlErqJg3ag6X5Iiz7GUYsuwb9UPWUOfBJ6z2j4F-GPlSyeoDHtq-2t7ncf1IvNgjtG5abxV976H5WMsubslVxHPrYAtlrbjBXJQwi7p_7Cb-7Xko7D_YJm_w8GxLGcTjorBH7n1DDv_2k=481899B1";
 
         try {
-            // Tworzenie połączenia HTTP z Jira REST API
+            // Creating an HTTP connection with Jira REST API
             String apiUrl = jiraUrl + "/rest/api/3/search?jql=project=" + projectKey;
             URL url = new URL(apiUrl);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -28,34 +28,34 @@ public class SantanderJiraAPI {
             int statusCode = connection.getResponseCode();
 
             if (statusCode == 200) {
-                // Odbieranie i przetwarzanie odpowiedzi
+                // Receiving and processing responses
                 String jsonResponse = new BufferedReader(new InputStreamReader(connection.getInputStream()))
                         .lines()
                         .collect(Collectors.joining(""));
 
-                // Przetwarzanie danych JSON
+                // JSON data processing
                 JSONObject json = new JSONObject(jsonResponse);
                 JSONArray issues = json.getJSONArray("issues");
 
-                // Wyświetlanie numeru zgłoszenia (key) i jego nazwy (summary)
+                // Displaying the issue key and its summary
                 for (int i = 0; i < issues.length(); i++) {
                     JSONObject issue = issues.getJSONObject(i);
                     String key = issue.getString("key");
                     String summary = issue.getJSONObject("fields").getString("summary");
 
-                    System.out.println("Numer zgłoszenia: " + key);
-                    System.out.println("Nazwa zgłoszenia: " + summary);
+                    System.out.println("Key: " + key);
+                    System.out.println("Summary: " + summary);
                     System.out.println();
                 }
             } else {
-                System.out.println("Błąd: " + statusCode);
+                System.out.println("Error: " + statusCode);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    // Metoda do generowania nagłówka autoryzacyjnego w formacie Basic Auth
+    // Method for generating authorization header in Basic Auth format
     private static String getBasicAuth(String username, String password) {
         String auth = username + ":" + password;
         return "Basic " + java.util.Base64.getEncoder().encodeToString(auth.getBytes());
